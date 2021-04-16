7446b5a2b2b880a03b15bad4199cd9fac29ef2bc
Pavel Sokolov <pasch.sockolow2012@yandex.ru>
2021-04-16 22:58:00 +0300

    refactor App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -7,35 +7,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        GameAlgorithm game;
+        GameAlgorithm game = null;
         String choice = getChoice();
-        if (choice.equals("1")) {
-            new GreetGame().greeting();
-        } else {
-            String userName = new GreetGame().greeting();
-            switch (choice) {
-                case "2":
-                    game = new EventGame();
-                    game.startGame(userName);
-                    break;
-                case "3":
-                    game = new CalcGame();
-                    game.startGame(userName);
-                    break;
-                case "4":
-                    game = new GCDGame();
-                    game.startGame(userName);
-                    break;
-                case "5":
-                    game = new ProgressionGame();
-                    game.startGame(userName);
-                    break;
-                case "6":
-                    game = new PrimeGame();
-                    game.startGame(userName);
-                    break;
-            }
+        switch (choice) {
+            case "1":
+                new GreetGame().greeting();
+                System.exit(0);
+            case "2":
+                game = new EventGame();
+                break;
+            case "3":
+                game = new CalcGame();
+                break;
+            case "4":
+                game = new GCDGame();
+                break;
+            case "5":
+                game = new ProgressionGame();
+                break;
+            case "6":
+                game = new PrimeGame();
+                break;
+            default:
+                System.exit(0);
         }
+        String userName = new GreetGame().greeting();
+        game.startGame(userName);
     }
 
     private static String getChoice() {
